32057590d7ee5436573a9cca0243dd69e07c5510
hermoz <herminiamozomoreno@gmail.com>
2020-05-06 20:51:26 +0200

    Importación módulos - FormsModule, HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TokenStorageComponent } from './services/token-storage/token-storage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TokenStorageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
