80d25eddfa6c1bc3b726485678617731a23a1661
hermoz <herminiamozomoreno@gmail.com>
2020-05-07 20:01:15 +0200

    Añadimos observable a interceptor

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpInterceptor, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HTTP_INTERCEPTORS } from '@angular/common/http';
 
+import { Observable } from 'rxjs';
 import { TokenStorageService } from '../services/token-storage.service';
 
 const TOKEN_HEADER_KEY = 'Authorization';
@@ -15,15 +16,18 @@ export class AuthInterceptor implements HttpInterceptor {
   /**
    * Inspects and transform HTTP requests before they are sent to the server
    */
-  intercept(request: HttpRequest<any>, next: HttpHandler) {
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let authRequest = request;
+    // Obtain token
     const token = this.tokenStorageService.getToken();
     if (token != null) {
+      // Important to clonate request
       authRequest = request.clone({
         /* Bearer is added to the token */
         headers: request.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + token)
       });
     }
+    // We move the modified request to the next interceptor in the chain
     return next.handle(authRequest);
   }
 }
