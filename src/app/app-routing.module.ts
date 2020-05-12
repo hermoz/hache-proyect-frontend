a1559848bb2123d4e6e2fcb476e8a5783f3db0f4
hermoz <herminiamozomoreno@gmail.com>
2020-05-12 21:40:21 +0200

    Routing of UsersComponent and UserDetailsComponent with data and canA…  …ctivate (guards)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,29 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 
+import { UsersDetailComponent } from './components/users-detail/users-detail.component';
+import { UsersComponent } from './components/users/users.component';
+import { AuthGuard } from './guards/auth.guard';
+import { READ_USERS } from './constants';
+
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+
+  {
+    path: 'users',
+    component: UsersComponent,
+    data: { requiredPrivileges: [ READ_USERS ] },
+    canActivate: [AuthGuard]
+  },
+
+  {
+  path: 'users/:id',
+    component: UsersDetailComponent,
+    data: { requiredPrivileges: [ READ_USERS ] },
+    canActivate: [AuthGuard]
+  },
 
   // routes that dont exists, we redirect to home
   { path: '**', redirectTo: '' }
