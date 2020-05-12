745fa9af9f66e04f6c6f69b2174394aef062539d
hermoz <herminiamozomoreno@gmail.com>
2020-05-12 22:59:34 +0200

    Update users.service.ts

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -11,13 +11,13 @@ import { API_USERS_ENDPOINT_URL, API_AUTH_ENDPOINT_URL, API_HTTP_OPTIONS } from 
 export class UsersService {
 
   constructor(private http: HttpClient) { }
+    /**
+     * Get list of users
+     * We use "pipe" over the Observable to catch the error using operator RxJS catchError()
+     * Observable can return multiple values, but An HttpClient Observable always emits a single value and ends
+     * Tap only registers the operation (on log)
+     */
 
-  /**
-   * Get list of users
-   * We use "pipe" over the Observable to catch the error using operator RxJS catchError()
-   * Observable can return multiple values, but An HttpClient Observable always emits a single value and ends
-   * Tap only registers the operation (on log)
-   */
   getUsers(): Observable<UserDto[]> {
     return this.http.get<UserDto[]>(API_USERS_ENDPOINT_URL)
       .pipe(
@@ -26,12 +26,13 @@ export class UsersService {
       );
   }
 
-  /**
-   * Delete user by id calls HttpClient.delete.
-   * The URL is the URL of the hero resource plus the hero id to delete
-   * (we keep sending httpOptions)
-   * @param id 
-   */
+    /**
+     * Delete user by id calls HttpClient.delete.
+     * The URL is the URL of the hero resource plus the hero id to delete
+     * (we keep sending httpOptions)
+     * @param id 
+     */
+
   deleteUser(id: number): Observable<any> {
     const url = `${API_USERS_ENDPOINT_URL}/${id}`;
     return this.http.delete<any>(url, API_HTTP_OPTIONS).pipe(
@@ -40,11 +41,12 @@ export class UsersService {
     );
   }
 
-  /**
-   * The following handleError() method reports the error and returns a harmless result
-   * so that the application continues to function
-   * @param error 
-   */
+    /**
+     * The following handleError() method reports the error and returns a harmless result
+     * so that the application continues to function
+     * @param error
+     */
+
   private handleError(error: HttpErrorResponse) {
     return throwError(error);
   }
