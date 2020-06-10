9e92a85e3c0c9e90ac3328f9d9d0ea0713cded56
hermoz <herminiamozomoreno@gmail.com>
2020-06-11 01:37:18 +0200

    Update constants.ts

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,7 +1,7 @@
 import { HttpHeaders } from '@angular/common/http';
 export const API_HTTP_OPTIONS = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
 
-export const API_URL = 'http://localhost:8080/api';
+export const API_URL = 'http://' + window.location.hostname + ':8080' + '/api';
 export const API_AUTH_ENDPOINT_URL = API_URL + '/auth/authenticate';
 export const API_USERS_ENDPOINT_URL = API_URL + '/users';
 export const API_ROLES_ENDPOINT_URL = API_URL + '/roles';
